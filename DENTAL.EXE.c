2036f62b187e8e38c954c3403a69b1f6cae93a49
plzburocho <133614539+plzburocho@users.noreply.github.com>
2023-05-14 21:30:33 -0400

    Add files via upload

---
+++ DENTAL.EXE.c
@@ -0,0 +1,37 @@
+typedef unsigned char   undefined;
+
+typedef unsigned int    word;
+typedef struct OLD_IMAGE_DOS_HEADER OLD_IMAGE_DOS_HEADER, *POLD_IMAGE_DOS_HEADER;
+
+struct OLD_IMAGE_DOS_HEADER {
+    char e_magic[2]; // Magic number
+    word e_cblp; // Bytes of last page
+    word e_cp; // Pages in file
+    word e_crlc; // Relocations
+    word e_cparhdr; // Size of header in paragraphs
+    word e_minalloc; // Minimum extra paragraphs needed
+    word e_maxalloc; // Maximum extra paragraphs needed
+    word e_ss; // Initial (relative) SS value
+    word e_sp; // Initial SP value
+    word e_csum; // Checksum
+    word e_ip; // Initial IP value
+    word e_cs; // Initial (relative) CS value
+    word e_lfarlc; // File address of relocation table
+    word e_ovno; // Overlay number
+};
+
+
+
+
+void __cdecl16far entry(void)
+
+{
+  undefined2 unaff_DS;
+  
+  *(undefined2 *)0xfffe = 0;
+  *(undefined2 *)0xfffc = unaff_DS;
+  *(undefined2 *)0xfffa = 0x10f;
+  return;
+}
+
+
